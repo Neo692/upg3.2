4151f1a3c3f085377bcaac9c14cdb684c279bcb0
nehy06001 <nehy06001@E-PW05CGQV>
2024-11-14 15:24:39 +0100

    Add project files.

---
+++ upg3.2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace upg2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Har du klarat gymnasiet? svara j för ja och n för nej");
+            string gymnasiet = Console.ReadLine();
+
+            Console.WriteLine("Hur gammal är du?");
+            int ålder = int.Parse(Console.ReadLine());
+
+            if (gymnasiet == "j" && ålder < 22)
+                Console.WriteLine("Vi vill anställa dig");
+            else
+            {
+                Console.WriteLine("Vi letar tyvärr efter annan personal");
+            }
+             
+            }
+        }
+    }
